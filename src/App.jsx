44abdd8e1aaefb21bbebd201cbdad5caad6b69ef
Filipov <andrijfilipov73@gmail.com>
2023-04-20 16:15:27 +0300

    make async redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-import {useState} from 'react'
-import { useDispatch } from 'react-redux';
-import {addTodo} from './store/TodoSlice'
-import './App.css';
-import TodoList from './components/TodoList';
-import InputField from './components/InputField';
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addNewTodo, fetchTodos } from './store/TodoSlice'
+import './App.css'
+import TodoList from './components/TodoList'
+import InputField from './components/InputField'
 
 function App() {
-
     const [text, setText] = useState('')
+    const { status, error } = useSelector((state) => state.todos)
     const dispatch = useDispatch()
 
     const addTask = () => {
-                    dispatch(addTodo({text}));
-                    setText('');
-                    }
+        dispatch(addNewTodo(text))
+        setText('')
+    }
 
-  return (
-    <div className="App">
-        <InputField 
-                    text={text}
-                    handleSubmit={addTask}
-                    handleInput={setText}
-        />
-      <TodoList />
-    </div>
-  );
+    useEffect(() => {
+        dispatch(fetchTodos())
+    }, [dispatch])
+
+    return (
+        <div className="App">
+            <InputField
+                text={text}
+                handleSubmit={addTask}
+                handleInput={setText}
+            />
+
+            {status === 'loading' && <h2>Loading...</h2>}
+            {error && <h2>An error occured: {error}</h2>}
+            <TodoList />
+        </div>
+    )
 }
 
-export default App;
+export default App
